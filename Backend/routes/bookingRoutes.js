b08a08cf32c748eb54751fca3770352928fc513e
eric1-fx <ericmanu670@gmail.com>
2026-05-25 09:16:26 +0000

    Remove Backend folder - frontend only

--- Backend/routes/bookingRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { bookRoom, getUserBooking } = require('../controllers/bookingController');
-
-router.post('/', bookRoom);
-router.get('/user/:userId', getUserBooking);
-
-module.exports = router;
